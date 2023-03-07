bb48b3e46ae381719372035186d559c851215344
Rafael Aguirre <reaguirre@cwti.com.co>
2023-03-07 14:40:45 -0500

    fix

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -10,8 +10,16 @@ export class OrderService {
     constructor(@InjectModel('Order') private readonly orderModels: Model<Order>){}
 
     async getOrders(): Promise<Order[]> {
-        const orders = await this.orderModels.find()
-        return orders
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); // Establecer la hora a las 00:00:00 para comparar solo la fecha
+      
+        const orders = await this.orderModels.find({
+          createDate: {
+            $gte: today,
+            $lt: new Date(today.getTime() + 24 * 60 * 60 * 1000) // Agregar 24 horas para incluir órdenes hasta el final del día
+          }
+        });
+        return orders;
     }
     async createOrder(createOrderDTO: CreateOrderDTO): Promise<Order> {
         const order = new this.orderModels(createOrderDTO)
